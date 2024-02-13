1b5af3f7a4b09ee5b29ad92663306fa4be4a1faf
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-02-13 01:21:07 -0300

    correciones

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,20 @@
-const fechaBoda = moment("2024-06-13 15:00:00");
+const fechaBoda = new Date("2024-07-13T15:00:00");
 
-function actualizarContador() {
-    const ahora = moment();
-    const tiempoRestante = moment.duration(fechaBoda.diff(ahora));
+        function actualizarContador() {
+            const ahora = new Date();
+            const diffTiempo = fechaBoda.getTime() - ahora.getTime();
 
-    document.getElementById('contador').innerText = tiempoRestante.days() + " Días  -  " + tiempoRestante.hours() + " Horas  -  " + tiempoRestante.minutes() + " Minutos restantes";
-}
+            if (diffTiempo <= 0) {
+                document.getElementById('contador').innerText = "¡Llegó el gran día!";
+            } else {
+                const meses = Math.floor(diffTiempo / (1000 * 60 * 60 * 24 * 30));
+                const dias = Math.floor((diffTiempo % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24));
+                const horas = Math.floor((diffTiempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                const minutos = Math.floor((diffTiempo % (1000 * 60 * 60)) / (1000 * 60));
 
-setInterval(function() {
-    actualizarContador();
-}, 1000);
+                document.getElementById('contador').innerText = `${meses} Meses, ${dias} Días, ${horas} Horas, ${minutos} Minutos restantes para la boda`;
+            }
+        }
+
+        actualizarContador();
+        setInterval(actualizarContador, 1000);
