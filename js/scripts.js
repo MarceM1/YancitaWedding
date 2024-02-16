d911d0aed60632e9b0165206affdb9a28ffc5dd2
Marcelo Melogno <marcelomelogno1986@gmail.com>
2024-02-16 20:37:03 -0300

    saco formulario

--- js/scripts.js
+++ js/scripts.js
@@ -28,3 +28,49 @@ function actualizarContador() {
 
 actualizarContador();
 setInterval(actualizarContador, 1000);
+
+// Scroll Reveal
+
+window.sr = ScrollReveal();
+
+sr.reveal(".toright", {
+  duration: 7500,
+  origin: "right",
+  distance: "-250px",
+});
+sr.reveal(".toleft", {
+  duration: 7500,
+  origin: "left",
+  distance: "-250px",
+});
+sr.reveal(".todown", {
+  duration: 5000,
+  origin: "bottom",
+  distance: "-200px",
+});
+sr.reveal(".toup", {
+  duration: 5000,
+  origin: "top",
+  distance: "-250px",
+});
+sr.reveal(".todown2", {
+  duration: 2500,
+  origin: "bottom",
+  distance: "-250px",
+});
+
+sr.reveal(".revealFast", {
+  duration: 1500,
+});
+
+sr.reveal(".reveal", {
+  duration: 4500,
+});
+
+sr.reveal(".revealSlow", {
+  duration: 9000,
+});
+
+sr.reveal(".revealExtraSlow", {
+  duration: 12000,
+});
